fba7ab7fb9457d9dfb2bfd5a60030f92d46d313a
Valeria <lerik13@gmail.com>
2021-12-08 11:18:36 -0700

    pass function 'onClick' as props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,14 @@
 import React, {FC} from 'react';
 
-export enum CardVariant { // Перечисление
+export enum CardVariant { // Enumeration
 	outlined = 'outlined',
 	primary  = 'primary'
 }
 interface CardProps {
 	width?: string;
 	height?: string;
-	//children?: React.ReactChild | React.ReactNode
 	variant: CardVariant;
+	onClick: () => void;
 }
 
 const Card: FC<CardProps> =
@@ -16,13 +16,16 @@ const Card: FC<CardProps> =
 		width, 
 		height,
 		variant,
+		onClick,
 		children
 	}) => {
 	return (
 		<div style={{width, height, 
 			border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
 			background: variant === CardVariant.primary ? 'lightgray' : ''
-		}}>
+		}}
+			onClick={onClick}
+		>
 			{children}
 		</div>
 	);
