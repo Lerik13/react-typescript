6ab421d7e4abbb38edc6fc96ce75bb4cf169ff1d
Valeria <lerik13@gmail.com>
2021-12-08 10:50:34 -0700

    add component Card with non-required fields + children

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Card from './components/Card';
 
 const App = () => {
 	return (
 		<div>
-			Start
+			<Card width='200px' height='200px'>
+				<button>Button</button>
+			</Card>
 		</div>
 	);
 };
