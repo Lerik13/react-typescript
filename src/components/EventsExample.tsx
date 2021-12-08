4440c6eb48f738f537c212b0fe73687e792e51bc
Valeria <lerik13@gmail.com>
2021-12-08 15:11:03 -0700

    typization hook 'useRef'

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,16 +1,17 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 
 const EventsExample: FC= () => {
-	const [value, setValue] = useState<string>('')
-	const [isDrag, setIsDrag] = useState<boolean>(false)
+	const [value, setValue] = useState<string>('');
+	const [isDrag, setIsDrag] = useState<boolean>(false);
+	const inputRef = useRef<HTMLInputElement>(null);
 
 	const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setValue(e.target.value)
 	}
 
 	const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-		console.log(value);
-		
+		//console.log(value);
+		console.log(inputRef.current?.value);
 	}
 
 	const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -33,7 +34,8 @@ const EventsExample: FC= () => {
 
 	return (
 		<div>
-			<input type="text" value={value} onChange={changeHandler} />
+			<input type="text" value={value} onChange={changeHandler} placeholder="Controlled" />
+			<input ref={inputRef} type="text" placeholder="Uncontrolled" />
 			<button onClick={clickHandler}>Event example</button>
 			<div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
 			<div 
